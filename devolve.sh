f1ddf4b243d76ce8d02ac34be02ea312f911fd41
Bart van Delft <vandeba@chalmers.se>
2015-02-12 17:17:29 +0100

    Devolve script finished, next adding webpage

--- devolve.sh
+++ devolve.sh
@@ -16,6 +16,7 @@ pdf="main"
 
 #
 # More tricky settings: depends on resize factor too.. now works only for A4.
+# Settings are very low-res, otherwise not feasible as web page animation.
 #  - pagew/pageh : Width / height of individual page after resizing
 #  - nrows/ncols : Number of rows / columns in generated picture
 #                  Keep in mind that older versions might be longer than the
@@ -27,52 +28,21 @@ pagew=179
 pageh=253
 nrows=3
 ncols=6
+onlyone="false"
+
+#
+# All done? Call the script as "./devolve.sh".
+#
 
 ### END OF SETTINGS ###
 
+#
 # Set up directories
+#
 mkdir -p tmp
 mkdir -p gifs
 mkdir -p website
-
 devolvedir=`pwd`
-
-# Go to latest commit:
-cd $repo
-git checkout $branch
-
-# Gather meta-information used on web page
-git log --date=local \
-  --pretty=format:'{%n "commit": "%H",%n "author": "%an",%n "email": "%ae",%n "date": "%ad",%n "message": "%f"%n},' $@ | \
-  perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
-  perl -pe 's/},]/}]/' > $devolvedir/website/metainfo.json
-  
-# History of all commit hashes
-git log --pretty=format:'%H' > $devolvedir/tmp/allcommits
-
-function combine {
-  cd $devolvedir/tmp/
-	ls
-  for r in $(seq 0 $(($nrows-1))); do 
-  cmd="convert "
-  for c in $(seq 0 $(($ncols-1))); do
-    cmd=$cmd"pdf-"$(($r*$ncols + $c))".png "
-  done
-  cmd=$cmd" +append row"$r".png"
-  { 
-    `$cmd` 
-  } 2> /dev/null
-  done
-  cmd="convert "
-  for r in $(seq 0 $(($nrows-1))); do 
-    cmd=$cmd"row"$r".png "
-  done
-  cmd=$cmd" -append all.png"
-  { 
-    `$cmd` 
-  } 2> /dev/null
-  cd $repo
-}
 
 #
 # Compute dimensions of final picture
@@ -80,19 +50,92 @@ function combine {
 finalw=$(($ncols*$pagew))
 finalh=$(($nrows*$pageh))
 dimensions=$finalw"x"$finalh
+
+#
+# Go to latest commit:
+#
+cd $repo
+git checkout $branch
+
+#
+# Gather meta-information to be used on web page
+#
+git log --date=local \
+  --pretty=format:'{%n "commit": "%H",%n "author": "%an",%n "email": "%ae",%n "date": "%ad",%n "message": "%f"%n},' $@ | \
+  perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
+  perl -pe 's/},]/}]/' > $devolvedir/website/metainfo.json
+
+#
+# History of all commit hashes.
+#
+git log --pretty=format:'%H' > $devolvedir/tmp/allcommits
+
+#
+# Function combining individual pictures from convert pdf.
+#
+function combine {
+  cd $devolvedir/tmp/
+	# Generating call combining groups of /ncols/ pages into rows.
+  for r in $(seq 0 $(($nrows-1))); do 
+		cmd="convert "
+		for c in $(seq 0 $(($ncols-1))); do
+			cmd=$cmd"pdf-"$(($r*$ncols + $c))".png "
+		done
+		cmd=$cmd" +append row"$r".png"
+		{ `$cmd` 
+		} 2> /dev/null
+  done
+	# Generating call combining /nrows/ pictures into one figure.
+  cmd="convert "
+  for r in $(seq 0 $(($nrows-1))); do 
+    cmd=$cmd"row"$r".png "
+  done
+  cmd=$cmd" -append all.png"
+  { `$cmd` 
+  } 2> /dev/null
+  cd $repo
+}
+
+#
+# Some user feedback.
+#
 ncommits=`grep -c '' $devolvedir/tmp/allcommits`
 n=0
 echo "Going through $ncommits commits (in reversed order)"
+
+#
+# Read commits one by one, make pdf, convert to pictures, combine pictures,
+# resize result and remove temporary files.
+#
 while read comhash; do
+  # User feedback:
   n=$(($n+1))
   echo "$n / $ncommits"
+	# Go to next commit, build pdf
 	git checkout $comhash
-	{ 
-    `$build` 
+	{ `$build` 
   } 2> /dev/null > /dev/null
+	# Convert pdf to png-s
   convert $pdf.pdf $devolvedir/tmp/pdf.png
+	# Combine png-s into single png
 	combine
+	# Convert png to resized, lower res gif.
   convert $devolvedir/tmp/all.png -resize 30% -flatten -type Grayscale -depth 3 -extent $dimensions $devolvedir/gifs/$comhash.gif
+	# Remove temp files.
   rm $devolvedir/tmp/*.png
-	break
+	if [ $onlyone = "true" ];
+	then
+	  break
+	fi
 done < $devolvedir/tmp/allcommits
+
+#
+# Reset repository.
+#
+cd $repo
+git checkout $branch
+
+#
+# User feedback.
+#
+echo "Done! Generated pictures can be found in gifs/ folder."
